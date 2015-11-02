fe86358babec9e8d5fbad0766edb9e4e064d5f7e
Krystal Raphael <krystal.raphael@gmail.com>
2015-11-02 13:18:08 -0800

    fixes before each block

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -72,7 +72,7 @@ describe('', function() {
 
     var requestWithSession = request.defaults({jar: true});
 
-var xbeforeEach = function(){};
+    xbeforeEach(function(){
       // create a user that we can then log-in with
       new User({
           'username': 'Phillip',
